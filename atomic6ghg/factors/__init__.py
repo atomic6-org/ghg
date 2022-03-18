5722d8a771b3bc3a5a68c03ed0686c3a0b6244c7
Mitchell, Mackenzie <Mackenzie.Mitchell@ge.com>
2022-03-18 15:25:05 -0400

    Implement migrate stationary combustion formulas

--- atomic6ghg/factors/__init__.py
+++ atomic6ghg/factors/__init__.py
@@ -1,6 +1,12 @@
 """ init """
+from .heat_content_factors import HeatContentFactors
+from .stationary_combustion_emission_factors import StationaryCombustionEmissionFactors
+from .refrigerants_gwp_factors import RefrigerantsGwpFactors
 from .unit_conversions_factors import UnitConversionsFactors
 from .molecular_weights_factors import MolecularWeightsFactors
 
+heat_content_factors = HeatContentFactors()
+stationary_combustion_emission_factors = StationaryCombustionEmissionFactors()
+refrigerants_gwp_factors = RefrigerantsGwpFactors()
 unit_conversions_factors = UnitConversionsFactors()
 molecular_weights_factors = MolecularWeightsFactors()
--- atomic6ghg/tests/conftest.py
+++ atomic6ghg/tests/conftest.py
@@ -8,6 +8,15 @@ import pytest
 
 
 @pytest.fixture
+def stationary_combustion_schema():
+    """Provides schema validation to tests"""
+    schema_file_contents = pkgutil.get_data("atomic6ghg.schemas", "stationary_combustion.json")
+    schema = json.loads(schema_file_contents)
+    v = Draft7Validator(schema=schema)
+    return v
+
+
+@pytest.fixture
 def waste_gases_schema():
     """Provides schema validation to tests"""
     schema_file_contents = pkgutil.get_data("atomic6ghg.schemas", "waste_gases.json")
