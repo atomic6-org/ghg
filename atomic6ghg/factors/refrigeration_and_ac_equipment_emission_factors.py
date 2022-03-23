64393f0318f54bbcc1edb95e920c7ddb45661c0e
Oh, Daniel <daniel.oh@ge.com>
2022-03-23 11:25:01 -0400

    Update comment in refrigeration_and_ac_equipment_emission_factors.py

--- atomic6ghg/factors/refrigeration_and_ac_equipment_emission_factors.py
+++ atomic6ghg/factors/refrigeration_and_ac_equipment_emission_factors.py
@@ -4,7 +4,7 @@ import pkgutil
 
 
 class RefrigerationAndAcEquipmentEmissionFactors:
-    """ Wrapper class for heat_content_factors.json """
+    """ Wrapper class for refrigeration_and_ac_equipment_emission_factors.json """
     def __init__(self):
         self.factors = json.loads(pkgutil.get_data('atom6ghg.factors',
             'source_data/refrigeration_and_ac_equipment_emission_factors.json'))
