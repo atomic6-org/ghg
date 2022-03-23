8ab0e445faeecc50a8d4af9ddd5114f1dcfb9734
Oh, Daniel <daniel.oh@ge.com>
2022-03-23 13:05:03 -0400

    Fix all atom6ghg reference to atomic6ghg

--- atomic6ghg/factors/refrigeration_and_ac_equipment_emission_factors.py
+++ atomic6ghg/factors/refrigeration_and_ac_equipment_emission_factors.py
@@ -6,7 +6,7 @@ import pkgutil
 class RefrigerationAndAcEquipmentEmissionFactors:
     """ Wrapper class for refrigeration_and_ac_equipment_emission_factors.json """
     def __init__(self):
-        self.factors = json.loads(pkgutil.get_data('atom6ghg.factors',
+        self.factors = json.loads(pkgutil.get_data('atomic6ghg.factors',
             'source_data/refrigeration_and_ac_equipment_emission_factors.json'))
 
     def __getitem__(self, item):
